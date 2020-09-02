9616928e3468bd791d18a4ec7f04b0fb17cfd4fb
jundic <jundi_chen@sfu.ca>
2020-09-01 22:39:15 -0700

    Updates

---
+++ precache-manifest.d2aeabaf38808c0480bcbffb2e367a66.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1844d92eba40a4199b0ab53fa5b9986c",
+    "url": "/eightDictionaryApp/index.html"
+  },
+  {
+    "revision": "c825c52f99b95a70283c",
+    "url": "/eightDictionaryApp/static/js/2.1df34c3b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/eightDictionaryApp/static/js/2.1df34c3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "547f49b6b7a8c69657ce",
+    "url": "/eightDictionaryApp/static/js/main.43d45236.chunk.js"
+  },
+  {
+    "revision": "e0e0630d13a28e5fcec2",
+    "url": "/eightDictionaryApp/static/js/runtime-main.42207a34.js"
+  },
+  {
+    "revision": "ceef5787e877b47b93c28a0340eef895",
+    "url": "/eightDictionaryApp/static/media/logo.ceef5787.png"
+  },
+  {
+    "revision": "ce030b85d28e3fa93630e496158459ae",
+    "url": "/eightDictionaryApp/static/media/search.ce030b85.png"
+  }
+]);
